638e49bae437fe767812330188f186f789ec091e
KaosMaker9 <nsifuentes09@gmail.com>
2025-04-22 22:30:57 -0500

    Posts
    Posts work or something kinda

--- static/js/app.js
+++ static/js/app.js
@@ -1,24 +1,38 @@
 const username = "admin";
 
 function renderPost(post) {
-    const template = document.getElementById("post-template").content.cloneNode(true);
-    template.querySelector(".username").innerText = post.username;
-    template.querySelector(".message").innerText = post.message;
-    document.getElementById("feed").appendChild(template);
+  const template = document
+    .getElementById("post-template")
+    .content.cloneNode(true);
+  template.querySelector(".username").innerText = post.username;
+  template.querySelector(".message").innerText = post.message;
+  document.getElementById("feed").appendChild(template);
 }
 
 function submitPost() {
-    const message = document.getElementById("postInput").value;
-    console.log("Would post:", message);
-    alert("Tweet submitted (not really yet)");
+  const message = document.getElementById("postInput").value;
+  try{
+    const response = fetch("/api/add_post", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            username, message
+        })
+    });
+  }catch (error)
+  {
+    console.log("Post failed 😭", error)
+  }
 }
 
 window.onload = async () => {
-    try{
-        const response = await fetch("/api/posts");
-        const posts = await response.json();
-        posts.forEach((post) => renderPost(post));
-    } catch(error) {
-        console.error("FIX THIS", error);
-    }
+  try {
+    const response = await fetch("/api/posts");
+    const posts = await response.json();
+    posts.forEach((post) => renderPost(post));
+  } catch (error) {
+    console.error("FIX THISSSS", error);
+  }
 };
